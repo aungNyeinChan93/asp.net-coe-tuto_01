49493753f83f3a06671a7f783fa28d1b73269b32
aungnyeinchan <aungnyeinchan.dev@gmail.com>
2026-01-04 22:44:11 +0630

    add employees

---
+++ Classes/Posts/Post.cs
@@ -0,0 +1,32 @@
+﻿namespace asp_tuto_01.Classes.Posts
+{
+    public class Post
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public Post(int id, string name , string desc)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Description = desc;
+        }
+    }
+
+    static class PostRepository
+    {
+        private static List<Post> _posts = new List<Post>()
+        {
+            new Post(1,"Post One","Post one desc"),
+            new Post(2,"Post Two","Post two desc"),
+            new Post(3,"Post Three","Post three desc"),
+        };
+
+        public static List<Post> GetAllPosts() => PostRepository._posts;
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using asp_tuto_01.Classes;
+using asp_tuto_01.Classes.Posts;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -34,6 +35,17 @@ app.Run(async (HttpContext context) =>
 
     if(context.Request.Path.StartsWithSegments("/employees") && context.Request.Method.Equals("GET") ){
         await context.Response.WriteAsync("Emplyoe list");
+        await context.Response.WriteAsync("\n \n");
+
+        var posts = PostRepository.GetAllPosts();
+
+        foreach (var post in posts)
+        {
+            await context.Response.WriteAsync($"Post Id :{post.Id}  \n");
+            await context.Response.WriteAsync($"Post name is {post.Name} \n");
+            await context.Response.WriteAsync($"Desc : {post.Description} \n ");
+
+        }
         return;
     }
 
